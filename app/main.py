84bddecc895b1f5c76eabafea3e6e990e9f29e0e
wenke.lai <wenke.studio@gmail.com>
2024-11-01 15:43:21 +0800

    feat: setup CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routers import users
 from .settings import SETTINGS_TYPE
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",  # local frontend
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    # Indicate that cookies should be supported for cross-origin requests
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router)
 
